41689a814e0102f7f68b73b750ba1e6b6e557523
Nikita Golik <nikitq4a@gmail.com>
2021-03-01 23:09:14 +0300

    request with params by using anootation RequestParam

--- src/main/java/by/golik/spring/controllers/FirstController.java
+++ src/main/java/by/golik/spring/controllers/FirstController.java
@@ -3,6 +3,9 @@ package by.golik.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Nikita Golik
@@ -12,7 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
